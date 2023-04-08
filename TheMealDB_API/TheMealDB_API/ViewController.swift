ea23cd8cf35e032e53eaffa5af0cb7b2bf243e3d
Dmitrii Kim <mitya_kim@icloud.com>
2023-04-07 20:28:59 -0700

    feat: Create TheMealDB API project

---
+++ TheMealDB_API/TheMealDB_API/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TheMealDB_API
+//
+//  Created by Mitya Kim on 4/7/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
